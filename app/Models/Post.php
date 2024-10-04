df6a3b5301df5c43b4fd7194b2fa60103e16eb79
yamakawakenichiro <nykenichiro@gmail.com>
2024-10-04 14:21:42 +0000

    ブログ投稿作成画面の保存処理実装

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,4 +14,8 @@ class Post extends Model
     public function getPaginateByLimit(int $limit_count = 10){
         return $this->orderBy('updated_at','DESC')->paginate($limit_count);
     }
+    protected $fillable = [
+        'title',
+        'body',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', [PostController::class,'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+Route::post('/posts', [PostController::class, 'store']);
