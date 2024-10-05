f280db672c2bc665cbc58b81a38cf8f38b095926
yamakawakenichiro <nykenichiro@gmail.com>
2024-10-05 12:23:55 +0000

    text

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 //この場合、App\Models内のPostクラスをインポートしている。
 use App\Models\Post;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
@@ -18,6 +18,45 @@ class PostController extends Controller
      */
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
     {
-        return $post->get();//$postの中身を戻り値にする。
+        // $test = $post->orderBy('updated_at', 'DESC')->limit(2)->toSql(); //確認用に追加
+        // dd($test); //確認用に追加
+        return view('posts.index')->with(['posts'=>$post->getPaginateByLimit()]);
+    }
+    /**
+     * 特定IDのpostを表示する
+     *
+     * @params Object Post // 引数の$postはid=1のPostインスタンス
+     * @return Reposnse post view
+     */
+    public function show(Post $post)
+    {
+    return view('posts.show')->with(['post' => $post]);
+    //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+
+        return redirect('/posts/' . $post->id);
+    }
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ Route::get('/posts', [PostController::class, 'index']);
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class,'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
