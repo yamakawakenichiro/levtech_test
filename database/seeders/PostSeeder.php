c618565d9fbdafd3ed0cf3dd38496124d925c9c5
Kenichiro Yamakawa <nykenichiro@gmail.com>
2024-10-05 19:22:27 +0900

    [add]マイグレーション

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use DateTime;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            'title' => '命名の心得',
+            'body' => '命名はデータを基準に考える',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('posts')->insert([
+            'title' => 'エラー文',
+            'body' => '読めるようになれば怖くない',
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+    }
+}
