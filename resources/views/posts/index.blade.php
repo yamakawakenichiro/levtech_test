4f0f48ebdb7f25bcbb811ed1d2e1940b92f00206
yamakawakenichiro_local <nykenichiro@gmail.com>
2024-10-12 20:16:24 +0900

    fix indes.にIndexヘッダーなし

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,45 +1,35 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-app-layout>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Blog</title>
-    <!--- Fonts --->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-</head>
-
-<body>
-    <h1>Blog Name</h1>
-    <a href='/posts/create'>create</a>
-    <div class='posts'>
-        @foreach ($posts as $post)
-        <div class='post">
+    <body>
+        <h1>Blog Name</h1>
+        <a href='/posts/create'>create</a>
+        <div class='posts'>
+            @foreach ($posts as $post)
+            <div class='post">
                 <h2 class=' title'>
-            <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-            </h2>
-            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-            <p class='body'>{{$post->body}}</p>
-            <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                @csrf
-                @method('DELETE')
-                <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
-            </form>
+                <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                </h2>
+                <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                <p class='body'>{{$post->body}}</p>
+                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                </form>
+            </div>
+            @endforeach
         </div>
-        @endforeach
-    </div>
-    <div class="paginate">
-        {{$posts->links()}}
-    </div>
-    <script>
-        function deletePost(id) {
-            'use strict'
+        <div class="paginate">
+            {{$posts->links()}}
+        </div>
+        <script>
+            function deletePost(id) {
+                'use strict'
 
-            if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
-                document.getElementById(`form_${id}`).submit();
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+                }
             }
-        }
-    </script>
-</body>
-
-</html>
+        </script>
+    </body>
+</x-app-layout>
