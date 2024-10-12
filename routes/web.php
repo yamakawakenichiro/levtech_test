4c8291a0ea3d5037453fea8f645ece28a7ba23c6
yamakawakenichiro_local <nykenichiro@gmail.com>
2024-10-12 20:04:31 +0900

    add ダッシュボードのナビゲーションにindex追加

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスを使えるようにします。
-use App\Http\Controllers\CategoryController;
-
-Route::get('/posts', [PostController::class, 'index']);
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,12 +14,21 @@ Route::get('/posts', [PostController::class, 'index']);
 |
 */
 
-Route::get('/', [PostController::class, 'index']);
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
-// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
-Route::post('/posts', [PostController::class, 'store']);
-Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
-Route::put('/posts/{post}', [PostController::class, 'update']);
-Route::delete('/posts/{post}', [PostController::class, 'delete']);
-Route::get('/categories/{category}', [CategoryController::class, 'index']);
+
+Route::get('/', [PostController::class, 'index'])->name('index');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
