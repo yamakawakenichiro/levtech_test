91f36a5ae763957f2b7ddce7d00656e269db71fa
yamakawakenichiro_local <nykenichiro@gmail.com>
2024-10-20 20:24:06 +0900

    docs: ルータとコントローラとビュー繋がり理解のため　コメント追加

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -5,6 +5,11 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
+/*
+**AppLayout.php は「シェフの指示書」**で、アプリ全体でどういう手順や共通部分を使うかを決めます。
+**app.blade.php は「お皿」**で、各ページのコンテンツ（料理）を共通の枠組みで表示するための「器」になります。
+*/
+
 class AppLayout extends Component
 {
     /**
--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,26 @@ use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 
+/* 
+auth: このミドルウェアは、ユーザーがログインしているかどうかをチェックします。
+ログインしていない場合は、ログインページにリダイレクトされます。
+verified: このミドルウェアは、ユーザーのメールアドレスが検証されているかどうかをチェックします。
+メールアドレスが検証されていない場合は、エラーが発生します。
+*/
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+/* 
+get(URL,メソッド)
+group：ルートを囲むように使用され、グループ内のすべてのルートに共通の設定を適用します。
+この場合、middleware(['auth']) はグループ内のすべてのルートに適用されます。
+Route::controllerはRoute クラスの controller という静的メソッドを呼び出し
+static（静的）メソッド：オブジェクトのインスタンスを作成せずに、クラスから直接呼び出すことができるメソッド
+::class は、クラス名を文字列として取得するための演算子。"App\Http\Controllers\PostController" という文字列
+*/
 Route::controller(PostController::class)->middleware(['auth'])->group(function () {
     Route::get('/', 'index')->name('index');
     Route::post('/posts', 'store')->name('store');
@@ -21,6 +36,12 @@ Route::controller(PostController::class)->middleware(['auth'])->group(function (
     Route::get('/posts/{post}/edit', 'edit')->name('edit');
 });
 
+
+/* 
+'/categories/{category}'：URLのパスと動的パラメータを定義。
+[CategoryController::class, 'index']：どのコントローラのメソッドが実行されるかを指定。
+->middleware('auth')：アクセス前に認証をチェックするミドルウェアを指定。
+*/
 Route::get('/categories/{category}', [CategoryController::class, 'index'])->middleware("auth");
 
 Route::middleware('auth')->group(function () {
@@ -29,4 +50,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+/* 
+__DIR__ は、現在のスクリプトファイル (web.php) が存在するディレクトリを指します。
+require ステートメントは、指定されたファイルを読み込み、その内容を現在のスクリプトファイルにインクルードします。
+
+*/
 require __DIR__ . '/auth.php';
